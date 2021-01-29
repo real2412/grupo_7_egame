b76e70799f919b13f11408bad2529f04663a130e
Renan <ramsp2412@gmail.com>
2021-01-28 19:41:13 -0500

    feat: add carrito

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require("express")
 const app = express()
 const path = require("path")
 
+app.use(express.static("public"))
+
 app.listen(3000, ()=>{
     console.log("Servidor Corriendo")
 })
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
+})
+
+app.get("/carrito", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/carrito.html"))
 })
