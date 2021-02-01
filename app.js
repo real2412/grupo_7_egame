6b8a282ec222c3fdb3ae97fb87108e096ae29729
max <mcisnerosc@uni.pe>
2021-02-01 15:55:35 -0500

    Agregando Inicio de sesion y registro

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const path = require("path")
 app.use(express.static("public"))
 
 app.listen(3000, ()=>{
-    console.log("Servidor Corriendo")
+    console.log("Servidor Corriendo en http://localhost:3000")
 })
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.get("/iniciar-sesion", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+
+app.get("/registro", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/signup.html"))
 })
 
 app.get("/carrito", (req, res) => {
